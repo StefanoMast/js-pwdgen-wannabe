4bdaf5daadf5f972dda213a3f8eb130f64572c22
StefanoMast <ste.mastrantonio@gmail.com>
2022-04-06 17:24:20 +0200

    add const nome, cognome, colore preferito e infine innerHTML

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const userName = prompt('Dimmi il tuo nome');
+console.log(userName);
+
+const userName1 = prompt('Dimmi il tuo cognome');
+console.log(userName1);
+
+const favColor = prompt ('Dimmi qual è il tuo colore preferito');
+console.log (favColor);
+
+document.getElementById('mytitle').innerHTML = nomecognomecolorepreferito21;
